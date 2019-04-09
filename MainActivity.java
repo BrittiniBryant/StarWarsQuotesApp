d6d205eaf69242cc99565207cd23d3f8dd66692a
Brittini Bryant <48459323+BrittiniBryant@users.noreply.github.com>
2019-04-09 19:13:37 -0400

    Update MainActivity.java
    Added code so that sound is played once the button is pressed.

--- MainActivity.java
+++ MainActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.support.annotation.RequiresApi;
+import android.media.MediaPlayer;
 
 public class MainActivity extends AppCompatActivity {
     private ImageButton quoteButton; //Created a private button and named it quoteButton
@@ -16,12 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        final MediaPlayer lightsabersound = MediaPlayer.create(this,R.raw.lightsabersound); //Final means that this cannot be changed.
 
         quoteTextView = findViewById(R.id.quoteText);
         quoteButton = findViewById(R.id.showQuoteButton);
         quoteButton.setOnClickListener(new View.OnClickListener(){
             @Override
                     public void onClick(View view) {
+                lightsabersound.start(); //This will play once the button is clicked.       
                 QuoteServer quoteServer = new QuoteServer();
                 quoteTextView.setText(quoteServer.getRandomQuote());
                 System.out.println("Hello"); //Whatever is here is what you will see on the console
